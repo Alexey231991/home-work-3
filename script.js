bb8da11f20a18e217c3fc5dafd680ca67b2e2093
Alexey231991 <lorketalex@ukr.net>
2021-02-22 17:57:39 +0200

    Done work

---
+++ script.js
@@ -0,0 +1,26 @@
+// первое задание
+let result = '';
+for (let index = 10; index < 20; index++) {
+    result += index + ', ';
+}
+
+console.log(result)
+
+// второе задание
+result = '';
+for (let index = 10; index <= 20; index++) {
+    result += index * index + ', ';
+}
+
+console.log(result)
+
+// третье задание
+for (let index = 1; index <= 10; index++) {
+    console.log(index + " * 7 = " + index * 7);
+}
+
+
+
+
+
+
